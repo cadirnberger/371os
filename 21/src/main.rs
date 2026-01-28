df42c45a43f3e8308ddf954872eb986e93f58fbb
cadirnberger <cadirnberger@willamette.edu>
2026-01-28 14:05:47 -0800

    update

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,13 +1,7 @@
-
 fn main() {
     let byte: [i32;3] = [0x6c6c6548i32, 0x6f77206fi32, 0x21646c72i32];
     unsafe {
-        println!(
-            "{}",
-            std::mem::transmute::<(usize,usize), &str>((
-                &byte as *const _ as usize,
-                12
-            ))
-        );
+        let st: &str =  std::mem::transmute::<(usize,usize), &str>((&byte as *const _ as usize,12));
+        println!("{}", st);
     }
 }
