ba0c5f960b2ecb7efabde793c5821eb37e510c88
cadirnberger <cadirnberger@willamette.edu>
2026-02-04 16:31:36 -0800

    RISC HW

--- 32/src/main.rs
+++ 32/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![allow(unconditional_recursion)]
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {} 
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    panic(info)
 }
